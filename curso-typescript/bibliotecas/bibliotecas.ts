b41b6ea8357df92924a51efc42d32ca01cb211dc
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-12-23 07:23:43 -0300

    jQuery

---
+++ curso-typescript/bibliotecas/bibliotecas.ts
@@ -0,0 +1,8 @@
+// p/ poder usar o JQUERY fazer a instalação do NPM
+// npm i --save-dev @types/jquery
+
+// declare const $: any
+
+$('body').append('Usando jQuery')
+// $('body').append(true)
+// $('body').
